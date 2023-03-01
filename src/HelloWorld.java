7458d2cba492ac65e98531e9df4662a4ea254870
tlaylock <LaylockTP18@gcc.edu>
2023-03-01 13:30:59 -0500

    I added a scanner

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main (String [] args){
-        System.out.println("Hello World");
+        Scanner s = new Scanner(System.in);
+        System.out.print("Give input:");
+        String name = s.next();
+        System.out.printf("Hello %s", name);
     }
+
 }
